312965b65520596f5e68d508a2ad46d1596a4c22
GeilsonFerreira <geilson.ferreira@gmail.com>
2025-12-18 07:42:29 -0300

    Update 01_alo_mundo.py

--- 01_alo_mundo.py
+++ 01_alo_mundo.py
@@ -1,1 +1,5 @@
-print("Alô, Mundo!")
+# Este é o primeiro código que se aprende quando começamos uma jornada de aprendizagem de qualquer linguagem de programação.
+
+# Vamos pedir ao sistema que exiba na tela uma mensagem
+
+print("Alô, mundo!")
